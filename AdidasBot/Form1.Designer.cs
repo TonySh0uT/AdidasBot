a53a3081a9ae20afac36b93af21244f927726a84
Sh0uTout <ebonitesh0ut@gmail.com>
2021-11-04 23:39:51 +0300

    v0.02 (Autologin and ordering without paying)

--- AdidasBot/Form1.Designer.cs
+++ AdidasBot/Form1.Designer.cs
@@ -29,20 +29,9 @@ namespace AdidasBot
         /// </summary>
         private void InitializeComponent()
         {
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // webBrowser1
-            // 
-            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(1661, 1131);
-            this.webBrowser1.TabIndex = 0;
-            this.webBrowser1.Url = new System.Uri("http://google.com", System.UriKind.Absolute);
             // 
             // textBox1
             // 
@@ -53,12 +42,13 @@ namespace AdidasBot
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(466, 72);
+            this.button1.Location = new System.Drawing.Point(454, 72);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(132, 47);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -67,7 +57,6 @@ namespace AdidasBot
             this.ClientSize = new System.Drawing.Size(1661, 1131);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.webBrowser1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -77,8 +66,6 @@ namespace AdidasBot
         }
 
         #endregion
-
-        private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
     }
--- AdidasBot/Form1.cs
+++ AdidasBot/Form1.cs
@@ -7,11 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Chrome.ChromeDriverExtensions;
+
 
 namespace AdidasBot
 {
     public partial class Form1 : Form
     {
+        IWebDriver browser;
+
+       
+
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +30,149 @@ namespace AdidasBot
         private void Form1_Load(object sender, EventArgs e)
         {
 
+            var chromeOptions = new OpenQA.Selenium.Chrome.ChromeOptions();
+             
+            chromeOptions.AddHttpProxy("46.150.254.217", 52879, "FU8cdVUd", "CuAjzPf1");
+         //   chromeOptions.AddArguments(new List<string>() { "no-sandbox", "headless", "disable-gpu" });
+            //    chromeOptions.AddArgument("disable-gpu");
+
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+
+
+
+
+
+
+            Console.WriteLine("Starting Web Driver");
+            browser = new OpenQA.Selenium.Chrome.ChromeDriver(chromeDriverService, chromeOptions);
+            Console.WriteLine("Web Driver started");
+            browser.Navigate().GoToUrl("https://www.adidas.ru/account-login");
+            //browser.Navigate().GoToUrl("https://2ip.ru/");
+
+
+
+            Thread.Sleep(1000);
+
+
+            IWebElement emailInput = browser.FindElement(By.Id("login-email"));
+            IWebElement passwordInput = browser.FindElement(By.Id("login-password"));
+            emailInput.SendKeys("TIMA.ARSLANOV.20@BK.RU");
+            passwordInput.SendKeys("Navi228131");
+
+            
+
+            Thread.Sleep(1000);
+
+            IWebElement loginBtn = browser.FindElement(By.CssSelector("[data-auto-id='login-form-login']"));
+            loginBtn.Click();
+
+            Thread.Sleep(1000);
+            browser.Navigate().GoToUrl("https://www.adidas.ru/krossovki-ozweego-celox/H04234.html");
+
+            Thread.Sleep(1000);
+
+            IWebElement sizeInput = browser.FindElement(By.XPath("//span[.='42 RU']"));
+            sizeInput.Click();
+
+
+
+            Thread.Sleep(1000);
+
+            IWebElement buy = browser.FindElement(By.XPath("//span[.='Добавить в корзину']"));
+            buy.Click();
+            Thread.Sleep(30000);
+            sizeInput.Click();
+            Thread.Sleep(1000);
+            buy = browser.FindElement(By.XPath("//span[.='Добавить в корзину']"));
+            buy.Click();
+            Thread.Sleep(3000);
+
+
+            IWebElement order = browser.FindElement(By.XPath("//span[.='ОФОРМИТЬ']"));
+            order.Click();
+
+            Thread.Sleep(3000);
+
+            IWebElement shipmentFName = browser.FindElement(By.CssSelector("[data-auto-id='shippingAddress-firstName']"));
+            IWebElement shipmentLName = browser.FindElement(By.CssSelector("[data-auto-id='shippingAddress-lastName']"));
+            IWebElement shipmentCity = browser.FindElement(By.CssSelector("[data-auto-id='shippingAddress-city']"));
+            IWebElement shipmentZipCode = browser.FindElement(By.CssSelector("[data-auto-id='shippingAddress-zipcode']"));
+
+            IWebElement shipmentStreet = browser.FindElement(By.CssSelector("[data-auto-id='shippingAddress-address1']"));
+            IWebElement shipmentHouse = browser.FindElement(By.CssSelector("[data-auto-id='shippingAddress-houseNumber']"));
+            IWebElement shipmentApartNum = browser.FindElement(By.CssSelector("[data-auto-id='shippingAddress-apartmentNumber']"));
+            IWebElement shipmentPhoneNum = browser.FindElement(By.CssSelector("[data-auto-id='shippingAddress-phoneNumber']"));
+
+            shipmentFName.SendKeys(OpenQA.Selenium.Keys.Control + "a");
+            shipmentFName.SendKeys(OpenQA.Selenium.Keys.Delete) ;
+
+            shipmentLName.SendKeys(OpenQA.Selenium.Keys.Control + "a");
+            shipmentLName.SendKeys(OpenQA.Selenium.Keys.Delete);
+
+            shipmentCity.SendKeys(OpenQA.Selenium.Keys.Control + "a");
+            shipmentCity.SendKeys(OpenQA.Selenium.Keys.Delete);
+
+            shipmentZipCode.SendKeys(OpenQA.Selenium.Keys.Control + "a");
+            shipmentZipCode.SendKeys(OpenQA.Selenium.Keys.Delete);
+
+            shipmentStreet.SendKeys(OpenQA.Selenium.Keys.Control + "a");
+            shipmentStreet.SendKeys(OpenQA.Selenium.Keys.Delete);
+
+            shipmentHouse.SendKeys(OpenQA.Selenium.Keys.Control + "a");
+            shipmentHouse.SendKeys(OpenQA.Selenium.Keys.Delete);
+
+
+            shipmentApartNum.SendKeys(OpenQA.Selenium.Keys.Control + "a");
+            shipmentApartNum.SendKeys(OpenQA.Selenium.Keys.Delete);
+
+
+            shipmentPhoneNum.SendKeys(OpenQA.Selenium.Keys.Control + "a");
+            shipmentPhoneNum.SendKeys(OpenQA.Selenium.Keys.Delete);
+
+
+
+
+
+
+
+
+
+
+
+
+            string FName = "Какое-то имя";
+            string LName = "Какая-то фамилия";
+            string City = "Город 1";
+            string Zip = "123456";
+            string Street = "Какая-то улица";
+            string House = "Какой-то дом";
+            string Apartament = "Какая-то квартира";
+            string Phone = "7777777777";
+
+            shipmentFName.SendKeys(FName);
+            shipmentLName.SendKeys(LName);
+            shipmentCity.SendKeys(City);
+            shipmentZipCode.SendKeys(Zip);
+            shipmentStreet.SendKeys(Street);
+            shipmentHouse.SendKeys(House);
+            shipmentApartNum.SendKeys(Apartament);
+            shipmentPhoneNum.SendKeys(Phone);
+
+
+
+            //  browser.Quit();
+            //  Application.Exit();
+
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
         }
     }
 }
